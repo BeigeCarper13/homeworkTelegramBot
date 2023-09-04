c0e7f792c86868a881c39f332738331223697948
Michael <misca1902@gmail.com>
2023-09-04 22:42:14 +0300

    ДОДЕЛАЛ

--- 1243выфп.py
+++ 1243выфп.py
@@ -11,20 +11,11 @@ conn = sql.connect(host="localhost", user="root", password="mySQL2341m", databas
 
 cursor = conn.cursor()
 
-cardslist = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-             '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-             '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
-             '31', '32', '33', '34', '35', '36', '37', '38', '39', '40',
-             '41', '42', '43', '44', '45', '46', '47', '48', '49', '50',
-             '51', '52', '53', '54']
-globalid = 0
-gameid = ''
-status = ''
-admin = ''
-players = []
-playerstext = ''
-queueu = ''
-playerq = ''
+lessonslist = [{'ГЕОГРАФ':'geogr'},{'БИОЛОГ':'biolog'},{'ФИЗИК':'phisic'},{'ХИМИЯ':'xim'},{'ТРУД':'trud'},
+               {'ИСКУССТВ':'isk'}]
+
+
+
 deletem = []
 
 bot = telebot.TeleBot('1876503650:AAH_sMeqFTVZx5PkW6dktrLKKJtIsPYkNck')
@@ -43,94 +34,152 @@ def start(message):
         status = str(row1)[2:len(row1) - 4]
 
     if status == '2':
-        if message.text == 'Предмет 1':
+        if message.text == 'АНГЛ ЯЗ':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btn1 = types.KeyboardButton("❌ Отменить")
-            markup.add(btn1)
-            deletem.append(bot.send_message(message.from_user.id, f"Напишите дз", reply_markup=markup))
-            work = '1'
-            bot.register_next_step_handler(message, adddz, work)
+            btn1 = types.KeyboardButton("1 Группа")
+            btn2 = types.KeyboardButton("2 Группа")
+            markup.add(btn1, btn2)
+            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            bot.register_next_step_handler(message, switchadd, 'eng1', 'eng2', '1 Группа', '2 Группа')
 
-        elif message.text == 'Предмет 2':
+        elif message.text == 'РУСС':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btn1 = types.KeyboardButton("❌ Отменить")
-            markup.add(btn1)
-            deletem.append(bot.send_message(message.from_user.id, f"Напишите дз", reply_markup=markup))
-            work = '2'
-            bot.register_next_step_handler(message, adddz, work)
+            btn1 = types.KeyboardButton("ЛИТРА")
+            btn2 = types.KeyboardButton("ЯЗЫК")
+            markup.add(btn1, btn2)
+            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            bot.register_next_step_handler(message, switchadd, 'rusl', 'rusy', 'ЛИТРА', 'ЯЗЫК')
 
-        elif message.text == 'Предмет 3':
+        elif message.text == 'БЕЛ':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btn1 = types.KeyboardButton("❌ Отменить")
-            markup.add(btn1)
-            deletem.append(bot.send_message(message.from_user.id, f"Напишите дз", reply_markup=markup))
-            work = '3'
-            bot.register_next_step_handler(message, adddz, work)
+            btn1 = types.KeyboardButton("ЛИТРА")
+            btn2 = types.KeyboardButton("ЯЗЫК")
+            markup.add(btn1, btn2)
+            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            bot.register_next_step_handler(message, switchadd, 'bell', 'bely', "ЛИТРА", 'ЯЗЫК')
+
+        elif message.text == 'МАТЕМ':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton("АЛГЕБР")
+            btn2 = types.KeyboardButton("ГЕОМЕТР")
+            markup.add(btn1, btn2)
+            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            bot.register_next_step_handler(message, switchadd, 'algebr', 'geom', "АЛГЕБР", 'ГЕОМЕТР')
+
+        elif message.text == 'ИНФОРМ':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton("1 ГРУППA")
+            btn2 = types.KeyboardButton("2 ГРУППA")
+            markup.add(btn1, btn2)
+            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            bot.register_next_step_handler(message, switchadd, '1g', '2g', "1 ГРУППA", '2 ГРУППA')
+
+        elif message.text == 'ИСТОРИЯ':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton("ВСЕМИР")
+            btn2 = types.KeyboardButton("БЕЛ")
+            markup.add(btn1, btn2)
+            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            bot.register_next_step_handler(message, switchadd, 'vsem', 'bel', "ВСЕМИР", 'БЕЛ')
+
+        elif message.text == 'ФИЗРА':
+            bot.send_message(message.from_user.id, "Если ты серьёзно хочешь написать дз по физре, то ты реал сумашедший")
+
+        elif message.text == 'ГЕОГРАФ' or message.text == 'БИОЛОГ' or message.text == 'ФИЗИК' or \
+                message.text == 'ХИМИЯ' or message.text == 'ТРУД' or message.text == 'ИСКУССТВ':
+            for i in lessonslist:
+                if str(message.text) == str(i.keys())[12:len(i.keys()) - 4]:
+                    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+                    btn1 = types.KeyboardButton("❌ Отменить")
+                    markup.add(btn1)
+                    deletem.append(bot.send_message(message.from_user.id, f"Напишите дз", reply_markup=markup))
+                    bot.register_next_step_handler(message, adddz, i[str(message.text)])
 
         elif message.text != 'Предмет 3':
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btn1 = types.KeyboardButton("Предмет 1")
-            btn2 = types.KeyboardButton("Предмет 2")
-            btn3 = types.KeyboardButton("Предмет 3")
-            markup.add(btn1, btn2, btn3)
-            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
-
+            lessons(message)
             conn.commit()
 
     elif status == '3':
         if message.text == 'АНГЛ ЯЗ':
-            switch(message, '1 ГРУППА', '2 ГРУППА')
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton("1 Группа")
+            btn2 = types.KeyboardButton("2 Группа")
+            markup.add(btn1, btn2)
+            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            bot.register_next_step_handler(message, switchwatch, 'eng1', 'eng2', '1 Группа', '2 Группа')
 
-        elif message.text == 'Предмет 2':
-            watch(message, '2')
+        elif message.text == 'РУСС':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton("ЛИТРА")
+            btn2 = types.KeyboardButton("ЯЗЫК")
+            markup.add(btn1, btn2)
+            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            bot.register_next_step_handler(message, switchwatch, 'rusl', 'rusy', 'ЛИТРА', 'ЯЗЫК')
 
-        elif message.text == 'Предмет 3':
-            watch(message, '3')
+        elif message.text == 'БЕЛ':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton("ЛИТРА")
+            btn2 = types.KeyboardButton("ЯЗЫК")
+            markup.add(btn1, btn2)
+            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            bot.register_next_step_handler(message, switchwatch, 'bell', 'bely', "ЛИТРА", 'ЯЗЫК')
+
+        elif message.text == 'МАТЕМ':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton("АЛГЕБР")
+            btn2 = types.KeyboardButton("ГЕОМЕТР")
+            markup.add(btn1, btn2)
+            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            bot.register_next_step_handler(message, switchwatch, 'algebr', 'geom', "АЛГЕБР", 'ГЕОМЕТР')
+
+        elif message.text == 'ИНФОРМ':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton("1 ГРУППA")
+            btn2 = types.KeyboardButton("2 ГРУППA")
+            markup.add(btn1, btn2)
+            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            bot.register_next_step_handler(message, switchwatch, '1g', '2g', "1 ГРУППA", '2 ГРУППA')
+
+        elif message.text == 'ИСТОРИЯ':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btn1 = types.KeyboardButton("ВСЕМИР")
+            btn2 = types.KeyboardButton("БЕЛ")
+            markup.add(btn1, btn2)
+            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            bot.register_next_step_handler(message, switchwatch, 'vsem', 'bel', "ВСЕМИР", 'БЕЛ')
+
+        elif message.text == 'ФИЗРА':
+            bot.send_message(message.from_user.id, "Если ты серьёзно ищешь дз по физре, то ты реал сумашедший")
+
+        elif message.text == 'ГЕОГРАФ' or message.text == 'БИОЛОГ' or message.text == 'ФИЗИК' or \
+                message.text == 'ХИМИЯ' or message.text == 'ТРУД' or message.text == 'ИСКУССТВ':
+            for i in lessonslist:
+                if str(message.text) == str(i.keys())[12:len(i.keys()) - 4]:
+                    watch(message, i[str(message.text)])
 
         elif message.text != 'Предмет 3':
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btn1 = types.KeyboardButton("Предмет 1")
-            btn2 = types.KeyboardButton("Предмет 2")
-            btn3 = types.KeyboardButton("Предмет 3")
-            markup.add(btn1, btn2, btn3)
-            bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup)
-
+            lessons(message)
             conn.commit()
 
     elif status == '1':
 
         if message.text == "✏ Добавить дз":
 
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btn1 = types.KeyboardButton("Предмет 1")
-            btn2 = types.KeyboardButton("Предмет 2")
-            btn3 = types.KeyboardButton("Предмет 3")
-            markup.add(btn1, btn2, btn3)
-            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            lessons(message)
 
             cursor.execute(f"""UPDATE `mydb`.`status` SET `status` = '2' WHERE (`id` = {message.from_user.id})""")
             conn.commit()
 
         elif message.text == '🔴 Посмотреть дз':
 
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btn1 = types.KeyboardButton("Предмет 1")
-            btn2 = types.KeyboardButton("Предмет 2")
-            btn3 = types.KeyboardButton("Предмет 3")
-            markup.add(btn1, btn2, btn3)
-            deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
+            lessons(message)
 
             cursor.execute(f"""UPDATE `mydb`.`status` SET `status` = '3' WHERE (`id` = {message.from_user.id})""")
             conn.commit()
 
         elif message.text == '/start':
+            basemenu(message)
 
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btn1 = types.KeyboardButton("✏ Добавить дз")
-            btn2 = types.KeyboardButton('🔴 Посмотреть дз')
-            btn3 = types.KeyboardButton('❔ как работает бот?')
-            markup.add(btn1, btn2, btn3)
-            deletem.append(bot.send_message(message.from_user.id, f"Выберите опцию", reply_markup=markup))
         elif message.text == '❔ как работает бот?':
             bot.send_message(message.from_user.id, f"Бот работает очень просто. Если вы хотите добавить домашнее "
                                                    f"задание, то нажмите '✏ Добавить дз'. Далее выберите предмет "
@@ -141,24 +190,55 @@ def start(message):
             bot.send_message(message.from_user.id, f"Для просмотра уже записанного дз нужно нажать кнопку"
                                                    f"'🔴 Посмотреть дз' и выбрать нужный предмет. Выведутся три "
                                                    f"последнее записанное дз.")
-
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btn1 = types.KeyboardButton("✏ Добавить дз")
-            btn2 = types.KeyboardButton('🔴 Посмотреть дз')
-            btn3 = types.KeyboardButton('❔ как работает бот?')
-            markup.add(btn1, btn2, btn3)
-            deletem.append(bot.send_message(message.from_user.id, f"Выберите опцию", reply_markup=markup))
+            basemenu(message)
         else:
+            basemenu(message)
 
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btn1 = types.KeyboardButton("✏ Добавить дз")
-            btn2 = types.KeyboardButton('🔴 Посмотреть дз')
-            btn3 = types.KeyboardButton('❔ как работает бот?')
-            markup.add(btn1, btn2, btn3)
-            deletem.append(bot.send_message(message.from_user.id, f"Выберите опцию", reply_markup=markup))
+def lessons(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton('АНГЛ ЯЗ')
+    btn2 = types.KeyboardButton("РУСС")
+    btn3 = types.KeyboardButton("БЕЛ")
+    btn4 = types.KeyboardButton("ИНФОРМ")
+    btn5 = types.KeyboardButton("ИСТОРИЯ")
+    btn6 = types.KeyboardButton("МАТЕМ")
+    btn7 = types.KeyboardButton("ИНФОРМ")
+    btn8 = types.KeyboardButton("ГЕОГРАФ")
+    btn9 = types.KeyboardButton("БИОЛОГ")
+    btn10 = types.KeyboardButton("ФИЗИК")
+    btn11 = types.KeyboardButton("ХИМИЯ")
+    btn12 = types.KeyboardButton("ТРУД")
+    btn13 = types.KeyboardButton("ИСКУСТВ")
+    btn14 = types.KeyboardButton("ФИЗРА")
+    markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12, btn13, btn14)
+    deletem.append(bot.send_message(message.from_user.id, "Выберите предмет", reply_markup=markup))
 
-def switch(message, one, two, ,):
-    pass
+def basemenu(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("✏ Добавить дз")
+    btn2 = types.KeyboardButton('🔴 Посмотреть дз')
+    btn3 = types.KeyboardButton('❔ как работает бот?')
+    markup.add(btn1, btn2, btn3)
+    deletem.append(bot.send_message(message.from_user.id, f"Выберите опцию", reply_markup=markup))
+def switchwatch(message, code1, code2, keyword1, keyword2):
+    if message.text == keyword1:
+        watch(message, code1)
+    if message.text == keyword2:
+        watch(message, code2)
+
+def switchadd(message, code1, code2, keyword1, keyword2):
+    if message.text == keyword1:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("❌ Отменить")
+        markup.add(btn1)
+        deletem.append(bot.send_message(message.from_user.id, f"Напишите дз", reply_markup=markup))
+        bot.register_next_step_handler(message, adddz, code1)
+    if message.text == keyword2:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("❌ Отменить")
+        markup.add(btn1)
+        deletem.append(bot.send_message(message.from_user.id, f"Напишите дз", reply_markup=markup))
+        bot.register_next_step_handler(message, adddz, code2)
 
 
 def adddz(message, work):
@@ -197,8 +277,6 @@ def adddz(message, work):
             cursor.execute(f"""SELECT homework From `mydb`.`student` WHERE (`predmet` = '{work}');""")
             rows = cursor.fetchall()
             for row in rows:
-                print(str(row)[2:len(row) - 4])
-                print(row)
                 cursor.execute(f"""DELETE From `mydb`.`student` WHERE (`homework` = '{str(row)[2:len(row) - 4]}');""")
                 break
 
@@ -212,7 +290,6 @@ def watch(message, work):
 
     rows = cursor.fetchall()
     for row in rows:
-        print(str(row)[2:len(row) - 4])
         cursor.execute(f"""SELECT people From `mydb`.`student` WHERE (`homework` = '{str(row)[2:len(row) - 4]}');""")
 
         rows1 = cursor.fetchall()
@@ -228,7 +305,7 @@ def watch(message, work):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("✏ Добавить дз")
     btn2 = types.KeyboardButton('🔴 Посмотреть дз')
-    btn3 = types.KeyboardButton('❔ как работает бот?')
+    btn3 = types.KeyboardButton('❔ Как работает бот?')
     markup.add(btn1, btn2, btn3)
     deletem.append(bot.send_message(message.from_user.id, f"Выберите опцию", reply_markup=markup))
 
